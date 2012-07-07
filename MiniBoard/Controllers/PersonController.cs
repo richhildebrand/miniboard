3a5ddc69b3c54ed66a338f5957590d4499cb2d99
Rich <Rich@rich.com>
2012-07-07 11:44:48 -0400

    added EF API Controller for Person

---
+++ MiniBoard/Controllers/PersonController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MiniBoard.Models;
+
+namespace MiniBoard.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private MiniBoardDBCotext db = new MiniBoardDBCotext();
+
+        // GET api/Person
+        public IEnumerable<Person> GetPeople()
+        {
+            return db.Person.AsEnumerable();
+        }
+
+        // GET api/Person/5
+        public Person GetPerson(int id)
+        {
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return person;
+        }
+
+        // PUT api/Person/5
+        public HttpResponseMessage PutPerson(int id, Person person)
+        {
+            if (ModelState.IsValid && id == person.personID)
+            {
+                db.Entry(person).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, person);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Person
+        public HttpResponseMessage PostPerson(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Person.Add(person);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, person);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = person.personID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Person/5
+        public HttpResponseMessage DeletePerson(int id)
+        {
+            Person person = db.Person.Find(id);
+            if (person == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Person.Remove(person);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, person);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MiniBoard/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+
+namespace MiniBoard.Models
+{
+   public class Person
+   {
+      public int personID { get; set; }
+      public string name { get; set; }
+   }
+
+   public class MiniBoardDBCotext : DbContext
+   {
+      public DbSet<Person> Person { get; set; }
+   }
+}
