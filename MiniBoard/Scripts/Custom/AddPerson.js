631878cfcffc476cb5683ba87e55956172f6739e
Rich <Rich@rich.com>
2012-07-07 15:34:00 -0400

    ready to start creating js add request

--- MiniBoard/Scripts/Custom/AddPerson.js
+++ MiniBoard/Scripts/Custom/AddPerson.js
@@ -1,3 +1,12 @@
-﻿$(".addPerson").click(function () {
+﻿var caught = function () {
+   alert("meh");
+};
+
+$('input').submit( function () {
+   return false;
+});
+
+$(".addPerson").click(function () {
    $("#PersonWindow").show();
 });
+
