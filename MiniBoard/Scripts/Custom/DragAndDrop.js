c5e0b8f6624265f70121b7a031777ebd09a77e8b
Rich <Rich@rich.com>
2012-07-08 16:05:05 -0400

    working drag and drop demo with target

--- MiniBoard/Scripts/Custom/DragAndDrop.js
+++ MiniBoard/Scripts/Custom/DragAndDrop.js
@@ -1,42 +1,26 @@
-﻿function draggableOnDragStart(e) {
-   $(".droptarget").text("(Drop here)");
-}
+﻿$(document).ready(function () {
+   var counter = 0;
 
-function droptargetOnDragEnter(e) {
-   $(".droptarget").text("Now you can drop it.");
-}
-
-function droptargetOnDragLeave(e) {
-   $(".droptarget").text("(Drop here)");
-}
-
-function droptargetOnDrop(e) {
-   $(".droptarget").text("You did great!");
-}
-
-function draggableOnDragEnd(e) {
-   var draggable = $(".draggable");
-
-   if (!draggable.data("kendoDraggable").dropped) {
-      // drag ended outside of any droptarget
-      $(".droptarget").text("Try again!");
+   var msg = function (text, e) {
+      debugger;
+      $('#msg').prepend(text + '<br/>');
    }
-}
 
-$(document).ready(function () {
-   $(".draggable").kendoDraggable({
-      hint: function (JQelement) {
-         return JQelement.clone();
-      },
-      dragstart: draggableOnDragStart,
-      dragend: draggableOnDragEnd
+   $('.draggable').kendoDraggable({
+      hint: function (item) {
+         return $(item).clone();
+      }
    });
 
-   $(".droptarget").kendoDropTarget({
-      dragenter: droptargetOnDragEnter,
-      dragleave: droptargetOnDragLeave,
-      drop: droptargetOnDrop
+   $('.droptarget').kendoDropTarget({
+      drop: function (e) { debugger; msg(counter++ + '. dropped on #one', e); }
    });
 
-   //var draggable = $(".draggable").data("kendoDraggable");
+   $('#two').kendoDropTarget({
+      drop: function (e) { msg(counter++ + '. dropped on #two'); }
+   });
 });
+
+
+// .droptarget
+// .draggable
